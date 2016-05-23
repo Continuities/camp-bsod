daa0b44a288d3ee2a89894b570845123a4bb531a
Continuities <michael@doublespeakgames.com>
2016-05-23 12:52:22 -0400

    Added light-switching support to camp crash

--- src/control-panel/index.js
+++ src/control-panel/index.js
@@ -13,11 +13,15 @@ var BOOT_TIME = 1000;
 var SOCKET = '/Users/vlc/vlc.sock';
 var REBOOT_SOUND = 'winxp.mp3';
 var TIME_REGEX = /\d+\s*$/;
+var ON = 0xff;
+var OFF = 0xfd;
 
 var express = require('express');
 var app = express();
 var exec = require('child_process').exec;
+var hid = require('node-hid');
 var crash_timer;
+var relay;
 
 function tell_vlc(command) {
   return syscall('echo "' + command + '" | nc -U ' + SOCKET);
@@ -32,7 +36,19 @@ function syscall(command) {
   });
 }
 
+function find_relay() {
+  return hid.devices().reduce((r,d) => {
+    if (r) { return r; }
+    if (d.product === 'USBRelay1') {
+      return new hid.HID(d.path);
+    }
+    return null;
+  }, null);
+}
+
 function init(port) {
+
+  relay = find_relay();
 
   app.use(express.static(__dirname + '/www'));
 
@@ -47,6 +63,7 @@ function init(port) {
       }, JITTER_TIME);
       res.status(200).send('crashed');
     });
+    relay && relay.write([0x0, ON, 1]);
   });
 
   app.post('/reboot', (req, res) => {
@@ -62,6 +79,7 @@ function init(port) {
               console.info('rebooted');
               res.status(200).send('rebooted');
             });
+        relay && relay.write([0x0, OFF, 1]);
       }, BOOT_TIME);
     });
   });
